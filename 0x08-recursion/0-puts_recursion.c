54111f20eb60eba4ce56387b697309e8750e9ae5
Lawal Mustapha Oluwatobi <mlawal308@gmail.com>
2023-10-29 22:28:02 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
-  * _puts_recursion - main function
+  * _puts_recursion - prints string
   *
-  * @s: function parameter
-  *
-  * Return: Always 0.
+  * @s: String to print
   */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
 
