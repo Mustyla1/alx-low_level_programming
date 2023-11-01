efd21814f044f5fb762560eb3de429d4d929aea8
Lawal Mustapha Oluwatobi <mlawal308@gmail.com>
2023-11-01 08:58:49 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiples two numbers
+* @argc: arguments count
+* @argv: argument vector
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
+
