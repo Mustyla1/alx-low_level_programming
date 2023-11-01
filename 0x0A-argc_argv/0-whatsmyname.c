f2da791045f9800a20baa546561e3d7c7620f28e
Lawal Mustapha Oluwatobi <mlawal308@gmail.com>
2023-11-01 08:33:30 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,13 @@
  * main - prints program name
  * @argc: argument count
  * @argv: argumen's vector
- * Return: 0 
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-        (void) argc;
+	(void) argc;
 
-        printf("%s\n", argv[0]);
-        return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 
 }
